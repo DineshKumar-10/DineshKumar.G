74e25751cd76c2d2c56417bf53fbaacfbe9dd233
Dinesh10 <dineshiam95@gmail.com>
2026-02-04 19:31:59 +0530

    animation_mobile

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent  implements OnInit, OnDestroy{
+  
+  roles: string[] = [
+    'Frontend Developer',
+    'Java Developer',
+    'Java Full Stack Developer',
+    'Backend Developer',
+    'Angular Developer'
+  ];
+
+  currentText = '';
+  private roleIndex = 0;
+  private charIndex = 0;
+  private isDeleting = false;
+  private typingSpeed = 120;
+  private deletingSpeed = 80;
+  private delayAfterWord = 1500;
+  private timeoutId: any;
+
+  ngOnInit(): void {
+    this.typeEffect();
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.timeoutId);
+  }
+
+  private typeEffect(): void {
+    const currentRole = this.roles[this.roleIndex];
+
+    if (!this.isDeleting) {
+      this.currentText = currentRole.substring(0, this.charIndex + 1);
+      this.charIndex++;
+    } else {
+      this.currentText = currentRole.substring(0, this.charIndex - 1);
+      this.charIndex--;
+    }
+
+    let delay = this.isDeleting ? this.deletingSpeed : this.typingSpeed;
+
+    if (!this.isDeleting && this.charIndex === currentRole.length) {
+      delay = this.delayAfterWord;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.charIndex === 0) {
+      this.isDeleting = false;
+      this.roleIndex = (this.roleIndex + 1) % this.roles.length;
+      delay = 500;
+    }
+
+    this.timeoutId = setTimeout(() => this.typeEffect(), delay);
+  }
 
 }
