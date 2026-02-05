f190f5efb658a2e02c154948826a2b9fb2c12a1c
Dinesh10 <dineshiam95@gmail.com>
2026-02-05 12:52:36 +0530

    skill_file_add

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,17 +1,20 @@
-import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-   standalone: true,
-  imports: [FormsModule,
-      HttpClientModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
 
+  isLoading = false;
+  successMsg = '';
+  errorMsg = '';
 
   formData = {
     fullName: '',
@@ -21,11 +24,28 @@ export class ContactComponent {
     message: ''
   };
 
+  private GOOGLE_SCRIPT_URL =
+    'https://script.google.com/macros/s/AKfycbxFn-R_jhQc4sXxfCnU97yu4kBRyhnQTqjzEmpf8YaZ-WVg0wWKutPFtdu50_0cfC45SQ/exec';
+
   constructor(private http: HttpClient) {}
 
-  onSubmit() {
-  alert('Message sent successfully!');
-}
+  onSubmit(form: any) {
+    if (form.invalid) return;
 
+    this.isLoading = true;
+    this.successMsg = '';
+    this.errorMsg = '';
 
+    this.http.post(this.GOOGLE_SCRIPT_URL, this.formData).subscribe({
+      next: () => {
+        this.successMsg = 'Message sent successfully ✅';
+        this.isLoading = false;
+        form.resetForm();
+      },
+      error: () => {
+        this.errorMsg = 'Something went wrong ❌ Try again.';
+        this.isLoading = false;
+      }
+    });
+  }
 }
